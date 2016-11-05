d721a47d6943f45c77f9cdf89ae43298c42afb42
Alfredo Eduardo <Alfredo Eduardo>
2016-11-05 17:35:12 -0200

    Commit

--- js/TempService.js
+++ js/TempService.js
@@ -1,5 +1,7 @@
 var TempService = (function() {
     function calcule(number) {
+        var result = 1;
+        if(number === 0) return result;
         return 120;
     }
     return {
--- test/TempServiceSpec.js
+++ test/TempServiceSpec.js
@@ -2,5 +2,10 @@ describe('TempService.js', function(){
 
     it ('Should calculate temp', function() {
         expect(TempService.calcule(50)).toBe(120);
+        expect(TempService.calcule(100)).toBe(212);
+        expect(TempService.calcule(150)).toBe(302);
+        expect(TempService.calcule(200)).toBe(392);
+        expect(TempService.calcule(250)).toBe(482);
+        expect(TempService.calcule(300)).toBe(572);
     });
 });
